5f3c85e79cc4eb9601e35bb004b255f1267a2457
JubiJung <devvjubi@gmail.com>
2024-06-23 18:41:13 +0900

    js homework

---
+++ mission00/mission00.js
@@ -0,0 +1,50 @@
+// 1. 문제 설명
+// 문제: 객체에서 특정 키의 값을 안전하게 가져오는 함수를 작성하세요.
+// 설명: 객체와 키를 인수로 받아, 객체에 해당 키가 존재하면 그 키에 해당하는 값을 반환하고, 존재하지 않으면 에러를 발생시키는 함수를 작성하세요.
+
+const person = {
+  name: "Alice",
+  age: 25,
+  city: "Wonderland",
+};
+
+function getValueAtObject(obj, key) {
+  // obj에 입력받은 key가 있으면 해당 키값 리턴
+  // 아니면 error
+  if (obj.hasOwnProperty(key)) {
+    return obj[key];
+  }
+  throw new Error("Error!");
+}
+
+console.log(getValueAtObject(person, "name")); // 'Alice'
+console.log(getValueAtObject(person, "age")); // 25
+console.log(getValueAtObject(person, "city")); // 'Wonderland'
+console.log(getValueAtObject(person, "country")); // Error !
+
+// 2. 문제 설명
+// 문제: 배열에서 특정 인덱스의 값을 안전하게 가져오는 함수를 작성하세요.
+// 설명: 배열과 인덱스를 인수로 받아, 인덱스가 배열의 유효한 범위 내에 있으면 그 인덱스에 해당하는 값을 반환하고, 유효하지 않은 인덱스일 경우 에러 메시지를 반환하는 함수를 작성하세요.
+
+// 추가 설명
+// - 배열의 인덱스는 0부터 시작하며, 주어진 인덱스가 배열의 유효한 범위 내에 있는지 확인해야 합니다.
+// - 인수로 받은 대상이 배열이 아닐 경우 에러를 생성하고 생성한 에러를 반환해야 합니다.
+// - 유효한 인덱스일 경우 해당 인덱스의 값을 반환하고, 그렇지 않으면  에러를 생성하고 생성한 에러를 반환해야 합니다.
+
+const numbers = [10, 20, 30, 40, 50];
+
+function getNumberAtArray(arr, index) {
+  // index의 숫자가 arr.length-1보다 크면 안됨
+  // arr의 타입이 배열이 아닐 경우 에러
+
+  // 경계조건 설정하기
+  if (Array.isArray(arr) && index >= 0 && index < arr.length) {
+    return arr[index];
+  }
+  throw new Error("Error!");
+}
+
+console.log(getNumberAtArray(numbers, 2)); // 30
+console.log(getNumberAtArray(numbers, 4)); // 50
+console.log(getNumberAtArray(numbers, 5)); // Error!
+console.log(getNumberAtArray(numbers, -1)); // Error!
